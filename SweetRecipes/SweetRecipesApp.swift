a21f21e58c7a439adb68f1c30c4139f8e276a6f0
Gray <grayzprogrammer@gmail.com>
2024-07-14 20:10:10 -0500

    initial app settings

--- SweetRecipes/SweetRecipesApp.swift
+++ SweetRecipes/SweetRecipesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SweetRecipesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipesHomeView()
         }
     }
 }
--- SweetRecipes/Views/RecipesHomeView.swift
+++ SweetRecipes/Views/RecipesHomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RecipesHomeView.swift
 //  SweetRecipes
 //
 //  Created by G Zhen on 7/14/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RecipesHomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RecipesHomeView()
 }
