67e9b8f3abd37855acb8e8875f86c5f9f223fc62
Gray <grayzprogrammer@gmail.com>
2024-07-14 20:42:13 -0500

    create required models and test data for preview env

---
+++ SweetRecipes/Models/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  SweetRecipes
+//
+//  Created by G Zhen on 7/14/24.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    let idMeal: String
+    let strMeal: String
+    let strMealThumb: String
+}
