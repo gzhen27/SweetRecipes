c3d70702502fcb56eee6853d8eaa6e51edc5a653
Gray <grayzprogrammer@gmail.com>
2024-07-14 20:07:03 -0500

    initial commit

---
+++ SweetRecipes/SweetRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetRecipesApp.swift
+//  SweetRecipes
+//
+//  Created by G Zhen on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SweetRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
